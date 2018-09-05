b1da51270d7006d7281fc541dcd3590bfa331663
root <henriquebento03@hotmail.com>
2018-09-05 15:45:53 -0400

    alteração de idade

--- ProjectsPython/TestGit.py
+++ ProjectsPython/TestGit.py
@@ -1,2 +1,5 @@
 test = input('Olá qual o seu nome? ')
-print('Olá {} Bem vindo ao seu primeiro progama em python, enviado ao GitHub'.format(test))
+ano = int(input('Que o ano vc nasceu? '))
+idade = 2018 - ano
+print('Olá {}, sua idade é {}. '.format(test, idade))
+print('Bem vindo ao seu primeiro progama em python, enviado ao GitHub')
