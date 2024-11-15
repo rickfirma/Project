49f2413f8cfc7c3cb4e7731586ca648a5e3dc072
rickfirma <42900675+rickfirma@users.noreply.github.com>
2024-11-15 08:45:38 -0300

    Delete ProjectsPython directory

--- ProjectsPython/TestGit.py
+++
@@ -1,5 +0,0 @@
-test = input('Olá qual o seu nome? ')
-ano = int(input('Que o ano vc nasceu? '))
-idade = 2018 - ano
-print('Olá {}, sua idade é {}. '.format(test, idade))
-print('Bem vindo ao seu primeiro progama em python, enviado ao GitHub')
